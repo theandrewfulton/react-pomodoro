b10db59de399b1ff9a96b60eb91044a4a98cb427
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-04-09 22:37:36 +1000

    add cssbassline for consistency

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-// import material-ui button
-import Button from '@mui/material/Button';
+// import material-ui components
+import CssBaseline from '@mui/material/CssBaseline'
+import Button from '@mui/material/Button'
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Hello World</Button>
+    </>
+  )
 }
 
 
-export default App;
+export default App
